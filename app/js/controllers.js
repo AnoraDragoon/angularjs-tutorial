6afb990c4a1a5f0f61d1f3a4f51c1240c3379672
Pérez González, Osvaldo <osvaldo.perezgonzalez@sicpa.com>
2022-11-23 09:10:28 +0100

    Tutorial step 6 - More Templating

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,14 +8,18 @@ phonecatControllers.controller('PhoneListCtrl', ['$scope', '$http',
     function ($scope, $http) {
         $http.get('phones/phones.json').success(function (data) {
             $scope.phones = data;
-        })
+        });
 
         $scope.orderProp = 'age';
     }
 ]);
 
-phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams',
-    function ($scope, $routeParams) {
-        $scope.phoneId = $routeParams.phoneId;
+phonecatControllers.controller('PhoneDetailCtrl', [
+    '$scope', '$routeParams', '$http',
+    function ($scope, $routeParams, $http) {
+        $http.get('phones/' + $routeParams.phoneId + '.json').success(function (data) {
+            $scope.phone = data;
+        });
+        // $scope.phoneId = $routeParams.phoneId;
     }
 ]);
